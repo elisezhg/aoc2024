fb47fbe31651fb62676f4a42f73f560d436a87f2
elisezhg <elise.zheng@hotmail.fr>
2024-12-03 00:21:42 -0500

    day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,13 +1,115 @@
-inputs_example = open("day03/day03_example.txt").read().split()
-inputs = open("day03/day03.txt").read().split()
+inputs_example = open("day03/day03_example.txt").read()
+inputs = open("day03/day03.txt").read()
 
 
 def part1(inputs):
-    return
+    total = 0
+    i = 0
+
+    while i < len(inputs):
+        valid = False
+        l, r = "", ""
+        s = i
+
+        commaFound = False
+
+        # left
+        if inputs[i:].startswith("mul("):
+            s += 4
+            while s < len(inputs):
+                if inputs[s].isnumeric():
+                    l += inputs[s]
+                    s += 1
+                elif inputs[s] == "," and l != "":
+                    commaFound = True
+                    s += 1
+                    break
+                else:
+                    s += 1
+                    break
+
+        if not commaFound:
+            i += 1
+            continue
+
+        # right
+        while s < len(inputs):
+            if inputs[s].isnumeric():
+                r += inputs[s]
+                s += 1
+            elif inputs[s] == ")" and r != "":
+                valid = True
+                s += 1
+                break
+            else:
+                s + 1
+                break
+
+        if valid:
+            total += int(l) * int(r)
+            i = s
+        else:
+            i += 1
+
+    return total
 
 
 def part2(inputs):
-    return
+    enabled = True
+    total = 0
+    i = 0
+
+    while i < len(inputs):
+        valid = False
+        l, r = "", ""
+        s = i
+
+        commaFound = False
+
+        if inputs[i:].startswith("don't()"):
+            enabled = False
+        elif inputs[i:].startswith("do()"):
+            enabled = True
+
+        # left
+        if inputs[i:].startswith("mul("):
+            s += 4
+            while s < len(inputs):
+                if inputs[s].isnumeric():
+                    l += inputs[s]
+                    s += 1
+                elif inputs[s] == "," and l != "":
+                    commaFound = True
+                    s += 1
+                    break
+                else:
+                    s += 1
+                    break
+
+        if not commaFound:
+            i += 1
+            continue
+
+        # right
+        while s < len(inputs):
+            if inputs[s].isnumeric():
+                r += inputs[s]
+                s += 1
+            elif inputs[s] == ")" and r != "":
+                valid = True
+                s += 1
+                break
+            else:
+                s + 1
+                break
+
+        if valid and enabled:
+            total += int(l) * int(r)
+            i = s
+        else:
+            i += 1
+
+    return total
 
 
 print("-------- Day 03 --------")
