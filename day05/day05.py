664e473be90cc743f3593495067f3b9c243b0e9a
elisezhg <elise.zheng@hotmail.fr>
2024-12-05 00:31:26 -0500

    day 5

--- day05/day05.py
+++ day05/day05.py
@@ -1,13 +1,96 @@
-inputs_example = open("day05/day05_example.txt").read().split()
-inputs = open("day05/day05.txt").read().split()
+inputs_example = open("day05/day05_example.txt").read().split("\n\n")
+inputs = open("day05/day05.txt").read().split("\n\n")
+from collections import defaultdict
 
 
 def part1(inputs):
-    return
+    _rules, updates = inputs[0].split("\n"), inputs[1].split("\n")
+
+    befores = defaultdict(list)
+    for r in _rules:
+        [a, b] = r.split("|")
+        befores[int(b)].append(int(a))
+
+    total = 0
+    for update in updates:
+        valid = True
+        update = update.split(",")
+        update = [int(i) for i in update]
+
+        seen = []
+
+        for num in update:
+            # all in seen must be in before
+            for s in seen:
+                if s not in befores[num]:
+                    valid = False
+                    break
+            seen.append(num)
+
+            if not valid:
+                break
+
+        if valid:
+            total += update[int((len(update) - 1) / 2)]
+
+    return total
 
 
 def part2(inputs):
-    return
+    _rules, updates = inputs[0].split("\n"), inputs[1].split("\n")
+    befores = defaultdict(list)
+
+    for r in _rules:
+        [a, b] = r.split("|")
+        befores[int(b)].append(int(a))
+
+    invalids = []
+    for update in updates:
+        valid = True
+        update = update.split(",")
+        update = [int(i) for i in update]
+
+        seen = []
+
+        for num in update:
+            # all in seen must be in before
+            for s in seen:
+                if s not in befores[num]:
+                    valid = False
+                    break
+            seen.append(num)
+
+            if not valid:
+                break
+
+        if not valid:
+            invalids.append(update)
+
+    total = 0
+
+    for nums in invalids:
+        corrected = correct(nums, befores)
+        total += corrected[int((len(corrected) - 1) / 2)]
+
+    return total
+
+
+def correct(nums, befores):
+    notSeen = [n for n in nums]
+    corrected = []
+
+    while notSeen:
+        for n in notSeen:
+            valid = True
+            for i in befores[n]:
+                if i in notSeen:
+                    valid = False
+                    break
+            if valid:
+                corrected.append(n)
+                notSeen.remove(n)
+
+    return corrected
 
 
 print("-------- Day 05 --------")
